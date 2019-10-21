7fde5785f9724b98e63e67c27291443afe8d3c9c
hassehulabeck <hansbudandersson@gmail.com>
2019-10-21 13:46:33 +0200

    First

--- matlagning.js
+++ matlagning.js
@@ -11,15 +11,16 @@ function lagaMat() {
     }, 1200);
 }
 
-function handlaMat(varor, callback) {
+function handlaMat(varor) {
     // Gå till affären och köp [varor]. Det tar en stund.
     setTimeout(function () {
         fridge.push(varor);
+        console.log("Nu har jag handlat färdigt");
     }, 2000);
-    callback();
 }
 
 let varor = ["potatis", "tomat", "linser"]
-handlaMat(varor, lagaMat);
+handlaMat(varor);
+lagaMat();
 
 // Vi måste först handla mat innan vi kan lagaMat.
