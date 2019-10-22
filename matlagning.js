c8261c2fd503cabc1609e030f0dce17f8b5e9a82
hassehulabeck <hansbudandersson@gmail.com>
2019-10-22 10:19:14 +0200

    changed to for-loop

--- matlagning.js
+++ matlagning.js
@@ -1,27 +1,42 @@
 var fridge = [];
 var desk = [];
+var matKasse = [];
 let varor = ["potatis", "tomat", "linser"]
 
-function lagaMat() {
-    // Laga mat.
-    fridge.forEach((vara) => {
+const lagaMat = () => {
+    var ingredienser = [];
+    matKasse.forEach((vara) => {
+        for (x = 0; x < vara.length; x++) {
+            // Hacka alla ingredienser...
+            ingredienser.push(vara[x]);
+        }
         desk.push(vara);
     });
-    setTimeout(() => {
-        console.log("Nu är maten färdig");
-    }, 1200);
+
+    for (i = 0; i < 500000000; i++) {
+        // Nothing, men det tar några sekunder. Settimeout funkar dåligt med promises...
+    }
+    return (ingredienser)
 }
+var handlaMat = new Promise((resolve, reject) => {
+    varor.forEach((vara) => {
+        matKasse.push(vara)
+    })
+    if (matKasse.length > 0) {
+        console.log("Nu har jag handlat mat")
+        resolve(matKasse);
+    } else {
+        reject("Inga pengar");
+    }
+});
 
-function handlaMat(varor) {
-    // Gå till affären och köp [varor]. Det tar en stund.
-    setTimeout(function () {
-        fridge.push(varor);
-        console.log("Nu har jag handlat färdigt");
-    }, 2000);
+const fridgeEmpty = function () {
+    if (fridge.length == 0) {
+        handlaMat
+            .then(lagaMat)
+            .then(fulfilled => console.log(fulfilled))
+            .catch(error => console.error(error.message))
+    }
 }
-
-
-var shoppa = handlaMat(varor);
-shoppa.then(lagaMat());
-
 // Vi måste först handla mat innan vi kan lagaMat.
+fridgeEmpty();
