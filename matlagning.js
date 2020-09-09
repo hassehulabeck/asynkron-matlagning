bfd96c3dba349d9b68325ce7cb62722277db9889
hassehulabeck <hansbudandersson@gmail.com>
2020-09-09 10:36:10 +0200

    Commented out timelogs

--- matlagning.js
+++ matlagning.js
@@ -3,7 +3,7 @@ var desk = [];
 var matKasse = [];
 let varor = ["Gurka", "Grädde", "fiskpinnar", "morot"]
 
-console.log(new Date().getTime());
+//console.log(new Date().getTime());
 
 const lagaMat = () => {
     return new Promise((resolve) => {
@@ -25,7 +25,7 @@ function handlaMat() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             varor.forEach((vara) => {
-                // matKasse.push(vara)
+                matKasse.push(vara)
             })
             if (matKasse.length > 0) {
                 resolve("Nu har jag handlat mat")
@@ -49,7 +49,7 @@ function diska() {
 // Async före funktionen, antingen som statement eller expression (var x = async function)
 async function fridgeEmpty() {
     if (fridge.length == 0) {
-        console.log(new Date().getTime());
+        //console.log(new Date().getTime());
         // Await framför den funktion, eller det kodblock du vill köra asynkront. Variabeln tar hand om det som returneras.
         const shop = await handlaMat();
         console.log(shop);
@@ -57,13 +57,13 @@ async function fridgeEmpty() {
         console.log(cook);
         const diskning = await diska();
         console.log(diskning);
-        console.log(new Date().getTime());
+        // console.log(new Date().getTime());
     }
 }
 
-console.log(new Date().getTime());
+//console.log(new Date().getTime());
 
 // Vi måste först handla mat innan vi kan lagaMat.
 fridgeEmpty().catch(err => console.error(err));
 
-console.log(new Date().getTime());
+//console.log(new Date().getTime());
